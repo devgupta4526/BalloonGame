e0d767b1af25b154a8ec6c601e290ff4736081e4
MimansaDeveloper <developer@mimansacare.com>
2024-11-05 13:47:51 +0530

    updated ui of the screens in game

--- frontend/src/components/BalloonStartScreen.jsx
+++ frontend/src/components/BalloonStartScreen.jsx
@@ -3,22 +3,22 @@ import buttonImage from "/imagebg.png"; // Image to be used as Play Now button
 
 const BalloonStartScreen = ({ startGame }) => {
   return (
-    <div className="absolute  z-[10]    inset-0 flex justify-center items-center w-full h-screen bg-black bg-opacity-20">
+    <div className="absolute  z-[10]    inset-0 flex justify-center items-center w-full h-screen bg-black bg-opacity-70">
       {/* Inner div styled with Tailwind */}
-      <div className=" w-[80%] h-[100vw] lg:w-[50vw] lg:h-[40vw]  bg-[#FFEBDA] rounded-[5vw] flex flex-col items-center justify-center shadow-xl p-[4vw] ">
+      <div className=" w-[80%] h-[100vw] lg:w-[50vw]  lg:h-[35vw]  bg-[#FFEBDA] rounded-[5vw] flex flex-col items-center justify-center shadow-xl p-[4vw] ">
          {/* Header with updated content */}
-         <h1 className="text-center mb-[3vw] font-poppins font-bold text-[9vw] lg:text-[5vw]  leading-none text-[#264195]">
+         <h1 className="text-center mb-[10vw] lg:mt-[2vw] mt-[5vw] lg:mb-[3vw] font-poppins font-bold text-[9vw] lg:text-[3vw]  leading-none text-[#264195]">
           How to Play
         </h1>
 
          {/* Updated instructions */}
       
-      <ul className="w-full  flex flex-col justify-center  text-black list-decimal px-[5vw]  lg:px-0 mb-[3vw] lg:text-[2vw] text-[3.5vw] font-bold" style={{ fontFamily: "'Comic Neue', sans-serif" }}>
-          <li className="mb-4">Press and hold the spacebar <br/>
-         <span className="text-blue-400">(mic gets activated)</span></li>
-          <li className="mb-4">Say Pop Pop.</li>
-          <li className="mb-4">Enjoy the balloon go pop</li>
-          <li className="mb-4">Press the spacebar and repeat</li>
+      <ul className="w-full text-left tracking-wider whitespace-nowrap  flex flex-col justify-center  text-black  px-[3vw]  lg:px-0 mb-[2vw] lg:mb-[0vw] lg:text-[2vw] text-[4vw] font-normal lg:font-normal" style={{ fontFamily: "'Comic Neue', sans-serif" }}>
+          <li className="mb-[1vw] lg:mb-[0.4vw] px-[2.5vw]">1. Press and hold the <span className="font-bold">spacebar</span>  <br/>
+         <span className="text-[#264195] px-[2.5vw]">(mic gets activated)</span></li>
+          <li className="mb-[1vw] lg:mb-[0.4vw] px-[2.5vw]">2. Say <span className="font-bold">Pop Pop...</span> </li>
+          <li className="mb-[1vw] lg:mb-[0.4vw] px-[2.5vw]">3. Enjoy the balloon go pop</li>
+          <li className="mb-[1vw] lg:mb-[0.4vw] px-[2.5vw]">4. Press the <span className="font-bold">spacebar</span>  and repeat</li>
         </ul>
      
 
@@ -26,17 +26,17 @@ const BalloonStartScreen = ({ startGame }) => {
 
         {/* Play Now Button (using image) */}
         <div
-          className="relative cursor-pointer active:scale-75"
+          className="relative  cursor-pointer active:scale-75"
           onClick={startGame}
         >
           <img
             src={buttonImage}
             alt="Play Now"
-            className="w-[250px] h-[80px] object-contain" // Adjust size for responsiveness
+            className="lg:w-[18vw] lg:h-[10vw] w-[50vw] h-[30vw] object-contain" // Adjust size for responsiveness
           />
           <span
-            className="absolute inset-0 flex justify-center items-center text-2xl text-white font-bold"
-            style={{ fontFamily: "'Comic Neue', sans-serif" }}
+            className="absolute  text-[#264195] inset-0 flex justify-center items-center text-[5vw] lg:text-[2vw]  font-bold"
+            style={{ fontFamily: "'Poppins', sans-serif" }}
           >
             Play Now
           </span>
--- frontend/src/components/Waitlist.jsx
+++ frontend/src/components/Waitlist.jsx
@@ -13,12 +13,12 @@ const Waitlist = ({playAgain}) => {
   };
 
   return (
-    <div className="absolute flex justify-center items-center w-full h-screen bg-black bg-opacity-20">
+    <div className="absolute flex justify-center items-center w-full h-screen bg-black bg-opacity-70">
       {/* Inner div styled with Tailwind */}
       <div className="absolute w-[80%] h-[100vw] lg:w-[50vw] lg:h-[40vw] bg-[#FFEBDA] rounded-[5vw] flex flex-col items-center justify-center shadow-xl p-[4vw]">
         
         {/* Header with updated content */}
-        <h1 className="text-center mb-[3vw] font-poppins font-bold text-[9vw] lg:text-[5vw]  leading-none text-[#264195]">
+        <h1 className="text-center mb-[8vw] lg:mt-0 mt-[2vw] lg:mb-[3vw] font-poppins font-bold text-[7vw] lg:text-[3vw]  leading-none text-[#264195]">
           That was fun!
         </h1>
 
@@ -28,13 +28,13 @@ const Waitlist = ({playAgain}) => {
         </button>
         
         {/* Updated instructions */}
-        <ul className="w-full text-black list-disc px-[5vw] lg:px-0 mb-[3vw] lg:text-[1.8vw] text-[3.5vw] font-bold" style={{ fontFamily: "'Comic Neue', sans-serif" }}>
-          <li className="mb-4">Playful sounds like 'pop' are key building blocks, helping toddlers connect actions to words and boost their speech skills</li>
-          <li className="mb-4">We're creating the best science-based speech games to help kids speak on time. Join our waitlist for early access to more fun games!</li>
+        <ul className="w-full tracking-tight text-black list-disc px-[5vw] lg:px-0 mb-[3vw] lg:text-[1.8vw] text-[3.5vw] font-normal" style={{ fontFamily: "'Comic Neue', sans-serif" }}>
+          <li className="mb-4">Playful sounds like <span className='font-bold'>'pop'</span> are key building blocks, helping toddlers connect actions to words and <span className='font-bold'>boost their speech skills</span></li>
+          <li className="mb-4">We're creating the <span className='font-bold'>best science-based speech games</span> to help kids speak on time. <span className='font-bold'>Join our waitlist for early access to more fun games!</span></li>
         </ul>
 
         {/* Buttons for "Play Again" and "Sign Up" */}
-        <div className="flex  gap-[2vw] lg:gap-[5vw] w-full h-[8vw] lg:h-[4vw]">
+        <div className="flex  gap-[2vw] lg:gap-[2vw] w-full h-[8vw] lg:h-[4vw]">
           <button 
             onClick={playAgain} 
             className="w-full border-2 border-pink-300 text-pink-300 hover:text-white hover:bg-pink-300 text-[2.5vw] lg:text-[1.5vw] whitespace-nowrap  font-bold py-[1vw] px-[3vw] rounded-lg lg:rounded-xl shadow-md"
